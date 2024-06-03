a59941c3c768edcc9b5cf3d9086c47eba4a4f6d2
Renata <renata.francisborges@gmail.com>
2024-06-02 21:54:28 -0300

    adjusting race condition

--- internal/processor/stress.go
+++ internal/processor/stress.go
@@ -11,6 +11,11 @@ import (
 	"golang.org/x/sync/semaphore"
 )
 
+type statusCodeCounter struct {
+	sync.Mutex
+	statusCodeCount map[int]int
+}
+
 func MakeStressTest(url string, requests int, concurrency int64) {
 
 	t := time.Now()
@@ -20,25 +25,27 @@ func MakeStressTest(url string, requests int, concurrency int64) {
 	sem := semaphore.NewWeighted(concurrency)
 	wg := &sync.WaitGroup{}
 
-	statusCodeCount := map[int]int{}
+	scc := &statusCodeCounter{
+		statusCodeCount: map[int]int{},
+	}
 
 	for i := 0; i < requests; i++ {
-
 		sem.Acquire(ctx, 1)
 		wg.Add(1)
 
 		go func() {
-
 			defer sem.Release(1)
 			defer wg.Done()
 
 			resp, err := http.Get(url)
 
-			_, exist := statusCodeCount[resp.StatusCode]
+			scc.Lock()
+			_, exist := scc.statusCodeCount[resp.StatusCode]
 			if !exist {
-				statusCodeCount[resp.StatusCode] = 0
+				scc.statusCodeCount[resp.StatusCode] = 0
 			}
-			statusCodeCount[resp.StatusCode]++
+			scc.statusCodeCount[resp.StatusCode]++
+			scc.Unlock()
 
 			if err != nil {
 				slog.Error("could not make request", "url", url, "error", err)
@@ -53,11 +60,11 @@ func MakeStressTest(url string, requests int, concurrency int64) {
 
 	fmt.Println("Total time spent on execution:", total)
 	fmt.Println("Total number of requests:", requests)
-	fmt.Println("Number of requests with HTTP status 200:", statusCodeCount[http.StatusOK])
+	fmt.Println("Number of requests with HTTP status 200:", scc.statusCodeCount[http.StatusOK])
 
-	delete(statusCodeCount, http.StatusOK)
+	delete(scc.statusCodeCount, http.StatusOK)
 
-	for statusCode, qt := range statusCodeCount {
+	for statusCode, qt := range scc.statusCodeCount {
 		ms := fmt.Sprintf("Distribution of other HTTP status codes (such as 404, 500, etc.) %d: Quantity:%d", statusCode, qt)
 		fmt.Println(ms)
 	}
